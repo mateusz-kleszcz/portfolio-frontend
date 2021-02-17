dc67715f6269552ce43d113b3f7d233daa2a8496
Mateusz Kleszcz <kleeeszcz@gmail.com>
2021-02-17 12:02:47 +0100

    attempt to fixed fonts

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,14 +1,25 @@
+import Head from 'next/head';
 import React, { FunctionComponent } from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
 const Layout: FunctionComponent = ({ children }) => {
     return (
-        <div className="container">
-            <Navbar />
-            {children}
-            <Footer />
-        </div>
+        <>
+            <Head>
+                <link
+                    rel='preload'
+                    href='http://fonts.googleapis.com/css?family=Lato:400,700'
+                    as="font"
+                    crossOrigin=""
+                />
+            </Head>
+            <div className="container">
+                <Navbar />
+                {children}
+                <Footer />
+            </div>
+        </>
     );
 };
 
