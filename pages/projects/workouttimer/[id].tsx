1eed491971e368f4aee087b8b4048ecc8470c649
mateusz-kleszcz <kleeeszcz@gmail.com>
2022-03-29 12:28:32 +0200

    attemp to deploy current version

--- pages/projects/workouttimer/[id].tsx
+++ pages/projects/workouttimer/[id].tsx
@@ -1,46 +1,44 @@
-import { GetStaticPropsContext } from 'next';
-import Timer from '@components/projects/workout-timer/Timer';
-import styles from '@styles/WorkoutTimer.module.scss'
-import { WorkoutWithID } from 'types/WorkoutTimer';
+import { GetStaticPropsContext } from "next";
+import Timer from "@components/projects/workout-timer/Timer";
+import styles from "@styles/WorkoutTimer.module.scss";
+import { WorkoutWithID } from "types/WorkoutTimer";
 
 interface TimerProps {
-    workout: WorkoutWithID
+  workout: WorkoutWithID;
 }
 
-export const getStaticPaths = async () => {
-    const res = await fetch(`${process.env.API_ADDRESS}/api/workout`)
-    const data = await res.json()
+// export const getStaticPaths = async () => {
+//     const res = await fetch(`${process.env.API_ADDRESS}/api/workout`)
+//     const data = await res.json()
 
-    const { workouts } = data
+//     const { workouts } = data
 
-    const paths = workouts.map((workout: WorkoutWithID) => {
-        return {
-            params: { id: workout._id }
-        }
-    })
+//     const paths = workouts.map((workout: WorkoutWithID) => {
+//         return {
+//             params: { id: workout._id }
+//         }
+//     })
 
-    return {
-        paths,
-        fallback: false
-    }
-}
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
 
-export const getStaticProps = async (context: GetStaticPropsContext) => {
-    const res = await fetch(`${process.env.API_ADDRESS}/api/workout/${context.params?.id}`)
-    console.log(res)
-    const data = await res.json()
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+//     const res = await fetch(`${process.env.API_ADDRESS}/api/workout/${context.params?.id}`)
+//     console.log(res)
+//     const data = await res.json()
 
-    return {
-        props: { workout: data }
-    }
-}
+//     return {
+//         props: { workout: data }
+//     }
+// }
 
 const timer = ({ workout }: TimerProps) => {
-    return (
-        <div className={styles.wortkoutTimer}>
-            <Timer {...workout} />
-        </div>
-    );
+  return (
+    <div className={styles.wortkoutTimer}>{/* <Timer {...workout} /> */}</div>
+  );
 };
 
 export default timer;
